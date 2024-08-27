c8294870bd67f83c5be3a3a266cd363cf65b9d4c
Taees Eimouri <taees.eimouri1@ibm.com>
2024-08-27 14:00:58 -0400

    Custom Parser

---
+++ src/main/java/com/ibm/guardium/universalconnector/commons/customparsing/CustomParser.java
@@ -0,0 +1,122 @@
+package com.ibm.guardium.universalconnector.commons.customparsing;
+
+import com.ibm.guardium.universalconnector.commons.customparsing.regex.RegexExecutor;
+import com.ibm.guardium.universalconnector.commons.customparsing.regex.RegexResult;
+import com.ibm.guardium.universalconnector.commons.structures.Accessor;
+import com.ibm.guardium.universalconnector.commons.structures.Record;
+import com.ibm.guardium.universalconnector.commons.structures.SessionLocator;
+import org.apache.commons.validator.routines.InetAddressValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.ibm.guardium.universalconnector.commons.customparsing.PropertyConstant.*;
+
+public abstract class CustomParser {
+    private static final Logger logger = LogManager.getLogger(RegexExecutor.class);
+    private static final RegexExecutor executor = new RegexExecutor();
+    static InetAddressValidator inetAddressValidator = InetAddressValidator.getInstance();
+    protected Map<String, String> properties;
+
+    public CustomParser() {
+    }
+
+    public Record parseRecord(String payload) {
+        properties = getProperties();
+        if (properties == null || payload == null) return null;
+
+        return extractRecord(payload);
+    }
+
+    private Record extractRecord(String payload) {
+        Record record = new Record();
+        if (properties.get(SESSION_ID) != null)
+            record.setSessionId(parse(payload, properties.get(SESSION_ID)));
+
+        if (properties.get(DB_NAME) != null)
+            record.setDbName(parse(payload, properties.get(DB_NAME)));
+
+        if (properties.get(CALLER_IP) != null)
+            record.setSessionLocator(parseSessionLocator(parse(payload, properties.get(CALLER_IP))));
+
+        record.setAccessor(parseAccessor("type", "hostname", "protocol", "serviceName", "appUserName"));
+
+        return record;
+    }
+
+    String parse(String payload, String regexString) {
+        Pattern pattern = Pattern.compile(regexString);
+        RegexResult rr = executor.find(pattern, payload);
+        if (rr.matched()) {
+            Matcher m = rr.getMatcher();
+            if (m.groupCount() > 0) return m.group(1);
+            return m.group();
+
+        } else {
+            if (rr.timedOut() && logger.isDebugEnabled()) {
+                logger.debug(
+                        String.format(
+                                "Regex parse aborted due to taking too long to match"
+                                        + " -- regex: %s, event-payload: %s",
+                                pattern, payload));
+            }
+            // Else, failed to parse without hitting timeout
+            return null;
+        }
+    }
+
+    Accessor parseAccessor(String serviceType, String hostname, String protocol, String serviceName, String appUserName) {
+        Accessor accessor = new Accessor();
+
+        accessor.setServerType(serviceType);
+        accessor.setServerOs(UNKOWN_STRING);
+
+        accessor.setClientOs(UNKOWN_STRING);
+        accessor.setClientHostName(UNKOWN_STRING);
+
+        accessor.setServerHostName(hostname);
+
+        accessor.setCommProtocol(UNKOWN_STRING);
+
+        accessor.setDbProtocol(protocol);
+        accessor.setDbProtocolVersion(UNKOWN_STRING);
+
+        accessor.setOsUser(UNKOWN_STRING);
+        accessor.setSourceProgram(UNKOWN_STRING);
+
+        accessor.setClient_mac(UNKOWN_STRING);
+        accessor.setServerDescription(UNKOWN_STRING);
+
+        accessor.setLanguage(Accessor.LANGUAGE_FREE_TEXT_STRING);
+        accessor.setDataType(Accessor.DATA_TYPE_GUARDIUM_SHOULD_NOT_PARSE_SQL);
+
+        accessor.setDbUser(appUserName);
+        accessor.setServiceName(serviceName);
+        return accessor;
+    }
+
+    SessionLocator parseSessionLocator(String callerIp) {
+        SessionLocator sessionLocator = new SessionLocator();
+        sessionLocator.setIpv6(false);
+        if (inetAddressValidator.isValidInet6Address(callerIp)) {
+            sessionLocator.setIpv6(true);
+            sessionLocator.setClientIpv6(callerIp);
+            sessionLocator.setServerIpv6(DEFAULT_IPV6);
+        } else if (inetAddressValidator.isValidInet4Address(callerIp)) {
+            sessionLocator.setClientIp(callerIp);
+            sessionLocator.setServerIp(DEFAULT_IP);
+        } else {
+            sessionLocator.setClientIp(DEFAULT_IP);
+            sessionLocator.setServerIp(DEFAULT_IP);
+        }
+        sessionLocator.setClientPort(SessionLocator.PORT_DEFAULT);
+        sessionLocator.setServerPort(SessionLocator.PORT_DEFAULT);
+        return sessionLocator;
+    }
+
+    public abstract Map<String, String> getProperties();
+
+}
---
+++ src/main/java/com/ibm/guardium/universalconnector/commons/customparsing/PropertyConstant.java
@@ -0,0 +1,22 @@
+package com.ibm.guardium.universalconnector.commons.customparsing;
+
+public class PropertyConstant {
+    public static final String SESSION_ID = "session_id";
+    public static final String DB_NAME = "db_name";
+    public static final String CLIENT_PORT = "client_port";
+    public static final String SERVER_PORT = "server_port";
+    public static final String DB_USER = "db_user";
+    public static final String SERVER_TYPE = "server_type";
+    public static final String DB_PROTOCOL = "db_protocol";
+    public static final String EXCEPTION_TYPE_ID = "exception_type_id";
+    public static final String UNKOWN_STRING = "";
+    public static final String DEFAULT_IPV6 = "0000:0000:0000:0000:0000:FFFF:0000:0000";
+    public static final String DEFAULT_IP = "0.0.0.0";
+    public static final String BIGTABLE_API = "bigtable.googleapis.com";
+    public static final String BIGTABLE_ADMIN_API = "bigtableadmin.googleapis.com";
+    public static final String PROTO_PAYLOAD = "protoPayload";
+    public static final String REQUEST_METADATA = "requestMetadata";
+    public static final String CALLER_IP = "callerIp";
+    public static final String SERVICE_NAME = "serviceName";
+    public static final String REQUEST = "request";
+}
